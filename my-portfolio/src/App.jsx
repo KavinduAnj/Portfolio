9e542c3f4ac8bad4843c7521e65b970265d7c8ec
Kavindu <wkanjana1590@gmail.com>
2025-08-28 00:12:10 +0530

    update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-     <div className='text-2xl'>Hello</div>
+      <Navbar />
+    
     </>
   )
 }
