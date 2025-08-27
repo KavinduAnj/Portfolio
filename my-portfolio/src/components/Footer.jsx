ac7ce83d90fefe835075a7296eaf06b49cf84451
Kavindu <wkanjana1590@gmail.com>
2025-08-28 00:47:29 +0530

    footer

---
+++ my-portfolio/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="bg-gray-900 text-white text-center py-4 fixed left-0 bottom-0 w-full">
+        <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+    </footer>
+);
+
+export default Footer;
