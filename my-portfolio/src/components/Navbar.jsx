9ef06ed693d98116d843d30153d741172fb86db3
Kavindu <wkanjana1590@gmail.com>
2025-09-06 22:55:56 +0530

    nav

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,46 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
   
     return (
         <nav className="sticky top-0 z-50 flex justify-between items-center px-8 bg-white py-4">
+          <div >
             <div className="font-bold text-3xl text-black">
               <Link to="/" >Kavindu Anjana</Link>
             </div>
-            <ul className="flex gap-6 list-none m-0 p-0">
-                <li>
-                    <Link 
-                      to="/" 
-                      className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold"
-                    >
-                      Home
-                    </Link>
-                </li>
-                <li>
-                  <Link to="/about"
-                  className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold"
-                  >
-                    About
-                  </Link>  
-                </li>
-                <li>
-                    <Link 
-                      to="/projects" 
-                      className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold"
-                    >
-                      Projects
-                    </Link>
-                </li>
-                <li>
-                    <Link 
-                      to="/contact" 
-                      className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold"
-                    >
-                      Contact
-                    </Link>
-                </li>
-            </ul>
+        <div className='hidden sm:block'>
+          <Link to="/" className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold">Home</Link>
+          <Link to="/about" className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold">About</Link>
+          <Link to="/projects" className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold"> Projects</Link>
+          <Link to="/contact" className="text-black text-2xl hover:text-blue-900 transition-colors hover:font-bold">Contact</Link>
+        </div> 
+        </div>  
         </nav>
     );
 };
