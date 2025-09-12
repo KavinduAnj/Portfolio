df7269de8745b7db5958a27f94f380577c333f78
Kavindu <wkanjana1590@gmail.com>
2025-09-13 00:19:17 +0530

    contacts updates

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -3,16 +3,16 @@ import { FaEnvelope, FaWhatsapp, FaFacebookMessenger } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center px-6 py-12">
+    <div className="min-h-screen flex items-center justify-center px-6 py-2">
       <div className="max-w-6xl w-full grid md:grid-cols-2 gap-12">
         {/* Left Side - Contact Info */}
         <div>
-          <h2 className="text-2xl font-semibold mb-8 text-center md:text-left">
+          <h2 className="text-2xl font-semibold mb-8 text-blue-700 text-center md:text-left">
             Talk to me
           </h2>
           <div className="space-y-6">
             {/* Email */}
-            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-gray-300 backdrop-blur-2xl">
+            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl">
               <FaEnvelope className="text-2xl text-gray-600" />
               <div>
                 <h3 className="font-semibold">Email</h3>
@@ -27,7 +27,7 @@ const Contact = () => {
             </div>
 
             {/* Whatsapp */}
-            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-gray-300 backdrop-blur-2xl">
+            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl">
               <FaWhatsapp className="text-2xl text-green-500" />
               <div>
                 <h3 className="font-semibold">Whatsapp</h3>
@@ -44,7 +44,7 @@ const Contact = () => {
             </div>
 
             {/* Messenger */}
-            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-gray-300 backdrop-blur-2xl">
+            <div className="border rounded-xl p-6 flex items-center gap-4 border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl">
               <FaFacebookMessenger className="text-2xl text-blue-500" />
               <div>
                 <h3 className="font-semibold">Messenger</h3>
@@ -64,7 +64,7 @@ const Contact = () => {
 
         {/* Right Side - Contact Form */}
         <div>
-          <h2 className="text-2xl font-semibold mb-8 text-center md:text-left">
+          <h2 className="text-2xl font-semibold mb-8 text-blue-700 text-center md:text-left">
             Write me your project
           </h2>
           <form className="space-y-6">
@@ -74,7 +74,7 @@ const Contact = () => {
               <input
                 type="text"
                 placeholder="Insert your name"
-                className="w-full border rounded-lg px-4 py-2 border-white/20 shadow-lg shadow-gray-200 backdrop-blur-2xl focus:ring-gray-400"
+                className="w-full border rounded-lg px-4 py-2 border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl focus:ring-gray-400"
               />
             </div>
 
@@ -84,7 +84,7 @@ const Contact = () => {
               <input
                 type="email"
                 placeholder="Insert your email"
-                className="w-full border rounded-lg px-4 py-2  border-white/20 shadow-lg shadow-gray-200 backdrop-blur-2xl focus:ring-gray-400"
+                className="w-full border rounded-lg px-4 py-2  border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl focus:ring-gray-400"
               />
             </div>
 
@@ -94,17 +94,16 @@ const Contact = () => {
               <textarea
                 rows="5"
                 placeholder="Write your project"
-                className="w-full border rounded-lg px-4 py-2  border-white/20 shadow-lg shadow-gray-200 backdrop-blur-2xl focus:ring-gray-400"
+                className="w-full border rounded-lg px-4 py-2  border-white/20 shadow-lg shadow-blue-100 backdrop-blur-2xl focus:ring-gray-400"
               ></textarea>
             </div>
 
             {/* Button */}
             <button
               type="submit"
-              className="w-full md:w-auto bg-black text-white px-6 py-3 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-800 transition"
+              className="w-full md:w-auto bg-blue-600 text-white px-6 py-3 rounded-lg flex items-center justify-center gap-2 hover:bg-blue-500 transition"
             >
               Send Message
-              <span>✈️</span>
             </button>
           </form>
         </div>
